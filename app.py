cf29e5dd2cf254397f0d8d5410020d7ecccf32fb
sethdiya-code <sethdia2706@gmail.com>
2026-04-09 20:47:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,21 @@ app = Flask(__name__)
 patients = []
 current_token = 0
 
+
 @app.route('/')
 def index():
     return render_template('index.html', patients=patients, current_token=current_token)
+
 
 @app.route('/add', methods=['POST'])
 def add_patient():
     name = request.form['name']
     phone = request.form['phone']
+
+    # ensure +91 format
+    if not phone.startswith('+'):
+        phone = '+91' + phone
+
     token = len(patients) + 1
 
     patients.append({
@@ -24,39 +31,48 @@ def add_patient():
 
     return redirect('/')
 
+
 @app.route('/next')
 def next_token():
     global current_token
+
     current_token += 1
-    
-    account_sid = "AC4b852228ce7c63a80942080ad72c30a5"
-    auth_token = "551694d10b5ff7bbc38336ba3db0554f "
+
+    account_sid = "AC4b852228ce7c63a80942080ad72c30a5 "
+    auth_token = "89ab4454e9e775a8a2a1e0f0b492a7f4"
+
     client = Client(account_sid, auth_token)
 
+    print("FUNCTION RUN HUA")
+
     try:
-    
         if current_token <= len(patients):
             phone = patients[current_token - 1]['phone']
+            print("Calling:", phone)
 
             call = client.calls.create(
-                url="http://demo.twilio.com/docs/voice.xml",
+                twiml='<Response><Say>Hello, your token number has come</Say></Response>',
                 to=phone,
                 from_="+17625258609"
             )
 
             print("CALL SID:", call.sid)
 
+        else:
+            print("No patient left")
+
     except Exception as e:
-        print("ERROR:", str(e))
+        print("💥 ERROR:", str(e))
 
     return redirect('/')
+
 
 @app.route('/delete/<token>')
 def delete_patient(token):
     global patients
     patients = [p for p in patients if str(p['token']) != str(token)]
     return redirect('/')
-    
+
 
 if __name__ == '__main__':
     app.run(debug=True)
