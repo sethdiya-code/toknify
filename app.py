4691f60f3acbd0b4905580743403324726cd951b
sethdiya-code <sethdia2706@gmail.com>
2026-04-12 13:41:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request, redirect
 from twilio.rest import Client
+import os
 
 app = Flask(__name__)
 
 patients = []
 current_token = 0
-
 
 @app.route('/')
 def index():
@@ -14,14 +14,10 @@ def index():
 
 @app.route('/add', methods=['POST'])
 def add_patient():
-    global current_token
-
-    current_token=0
-    
     name = request.form['name']
     phone = request.form['phone']
 
-    
+
     if not phone.startswith('+'):
         phone = '+91' + phone
 
@@ -40,28 +36,29 @@ def add_patient():
 def next_token():
     global current_token
 
+    current_token += 1
 
-    account_sid = "AC4b852228ce7c63a80942080ad72c30a5 "
-    auth_token = "daba2ec69e4ca4828c8bce847f67718c"
+
+    account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+    auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+
+    print("SID:", account_sid)
+    print("TOKEN:", auth_token)
 
     client = Client(account_sid, auth_token)
 
-    print("FUNCTION RUN HUA")
-
     try:
         if current_token <= len(patients):
-            phone = patients[current_token]['phone']
+            phone = patients[current_token - 1]['phone']
             print("Calling:", phone)
 
             call = client.calls.create(
                 twiml='<Response><Say>Hello, your token number has come</Say></Response>',
                 to=phone,
-                from_="+17625258609"
+                from_='+17625258609'
             )
 
             print("CALL SID:", call.sid)
-
-            current_token+=1
 
         else:
             print("No patient left")
@@ -79,5 +76,5 @@ def delete_patient(token):
     return redirect('/')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
