53a5ec426ebcf8ddaa9abc20e1a51880677c284a
sethdiya-code <sethdia2706@gmail.com>
2026-04-13 13:05:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,18 @@ import os
 
 app = Flask(__name__)
 
+
 patients = []
 current_token = 0
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', patients=patients, current_token=current_token)
+    return render_template(
+        'index.html',
+        patients=patients,
+        current_token=current_token
+    )
 
 
 @app.route('/add', methods=['POST'])
@@ -34,70 +39,44 @@ def add_patient():
 
 
 
-def make_call(phone, message):
+@app.route('/call/<int:token>')
+def call_patient(token):
+    global current_token
+
+    
+    current_token = token
+
     account_sid = os.getenv("TWILIO_ACCOUNT_SID")
     auth_token = os.getenv("TWILIO_AUTH_TOKEN")
 
     client = Client(account_sid, auth_token)
 
-    try:
-        client.calls.create(
-            twiml=f'<Response><Say>{message}</Say></Response>',
-            to=phone,
-            from_="+17625258609"
-        )
-        print("CALL SUCCESS:", phone)
-
-    except Exception as e:
-        print("CALL ERROR:", str(e))
-
-
-@app.route('/next')
-def next_token():
-    global current_token
-    current_token += 1
-    return redirect ('/')
-
-    print("FUNCTION RUN HUA")
-
-    try:
-    
-        if current_token <= len(patients):
-            phone = patients[current_token - 1]["phone"]
-            make_call(phone, "Please come, it is your turn")
-
-        else:
-            print("No patient left")
-
-    except Exception as e:
-        print("ERROR:", str(e))
-
-    return redirect('/')
-
-
-
-@app.route('/call/<token>')
-def call_patient(token):
     for p in patients:
-        if str(p["token"]) == str(token):
-            phone = p["phone"]
-            print("Manual Call:", phone)
+        if p['token'] == token:
+            phone = p['phone']
 
-            make_call(phone, "Please come, it is your turn")
+            print("Calling:", phone)
+
+            call = client.calls.create(
+                twiml="<Response><Say>Hello, your token number has come</Say></Response>",
+                to=phone,
+                from_="+17625258609"
+            )
+
+            print("CALL SID:", call.sid)
             break
 
     return redirect('/')
 
 
-@app.route('/delete/<token>')
+
+@app.route('/delete/<int:token>')
 def delete_patient(token):
     global patients
 
-    patients = [p for p in patients if str(p['token']) != str(token)]
+    patients = [p for p in patients if p['token'] != token]
 
     return redirect('/')
-
-
-
+    
 if __name__ == '__main__':
     app.run(debug=True)
